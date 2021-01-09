3115f7406fa85e7651a2680f88a764419d835a46
AhmedMensh <ahmedmensh8@gmail.com>
2021-01-09 08:32:19 +0200

    edit data model

--- app/src/main/java/com/android/company/app/androidtask/models/Food.kt
+++
@@ -1,9 +0,0 @@
-package com.android.company.app.androidtask.models
-
-typealias Foods = List<Food>
-data class Food(
-    val id: Int? = null,
-    val name: String? = null,
-    val price: Float? = null,
-    val url: String? = null
-)
